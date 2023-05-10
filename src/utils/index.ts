1a50e06888895e7903a87a5d90fe2d98a66979cd
legendary <1207588603@qq.com>
2023-05-10 11:04:21 +0800

    新增延时函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,11 @@
 // 将对象序列化成 URL 的形式
 export function toUrlEncode(data: { [key: string]: any }): string {
   const list: string[] = []
-  for (const key in data) {
-    list.push(`${key}=${data[key]}`)
-  }
+  for (const key in data) list.push(`${key}=${data[key]}`)
   return list.join('&')
+}
+
+// 延时函数
+export function sleep(delay: number): Promise<void> {
+  return new Promise((resolve, reject) => setTimeout(() => resolve(), delay))
 }
